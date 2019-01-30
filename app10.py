7847c5309a77da6c6b3083c35f0cb5ba122c8d66
girishenoy95 <girishenoy95@gmail.com>
2019-01-30 17:39:00 +0530

    minor commit

---
+++ app10.py
@@ -0,0 +1,18 @@
+# Playing around with 2D Lists and Nested Loops
+
+number_list = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+
+for row in number_list:
+    for column in row:
+        print(column, end=" ")
+    print()
+
+# The output is
+#  1 2 3
+#  4 5 6
+#  7 8 9
