9de3e1ceac4400d81a0f00b6f9a1c7263270dd88
girishenoy95 <girishenoy95@gmail.com>
2019-01-30 13:00:20 +0530

    minor commit

---
+++ app9.py
@@ -0,0 +1,54 @@
+# Playing around with For Loops
+
+
+for letter in "Giridhar Shenoy":
+    print(letter)
+
+# The output for above will be
+# G
+# i
+# r
+# i
+# d
+# h
+# a
+# r
+
+# S
+# h
+# e
+# n
+# o
+# y
+
+
+friends = ['raja', 'ram', 'mohan', 'roy']
+
+for friend in friends:
+    print(friend)
+
+# Output for above will be
+# raja
+# ram
+# mohan
+# roy
+
+
+for friend in range(len(friends)):
+    print(friends[friend])
+
+# Output for above will be
+# raja
+# ram
+# mohan
+# roy
+
+
+for index in range(10):
+    print(index)   # Will print 0-9 vertically
+
+
+for index in range(3, 10):
+    print(index)  # Will spit out numbers from 3 to 9
+
+
