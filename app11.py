525ee547902ab6838ad015ca9f7f208ce6c3e93e
girishenoy95 <girishenoy95@gmail.com>
2019-02-01 11:33:45 +0530

    minor commit

---
+++ app11.py
@@ -0,0 +1,17 @@
+# Playing with try/except bloc i.e similar to try catch block
+
+# Example 1
+try:
+    number=int(input("Please enter a number"))
+    print(number)
+except:  # Will catch any exception  (Very Broad definition for except)
+    print("Please input a int value")
+
+
+# Example 2
+try:
+    di = 10/0
+    print("Number divided by zero")
+except ZeroDivisionError:   # Will catch a specific exception
+    print("Cannot divide by zero")
+
